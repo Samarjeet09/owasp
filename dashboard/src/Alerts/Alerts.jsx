f4bf7af46169bcab4ec8987616769438737f2d89
Himanshu <hbansal_be21@thapar.edu>
2024-04-06 05:30:36 +0530

    Alert page done, camera sleep, crud op

--- dashboard/src/Alerts/Alerts.jsx
+++ dashboard/src/Alerts/Alerts.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import DataBox from '../DataBox/DataBox'
+// import DataBox from '../DataBox/DataBox'
 import './Alerts.css'
+import AlertDataBox from '../DataBox/AlertDataBox'
 
 const Alerts = () => {
     return (
         <div>
             <div className='databox'>
-                <DataBox name={'Patient Out'} />
-                {/* <DataBox name={'Appointments'} /> */}
+                <AlertDataBox name={'Patient Out'} />
             </div>
         </div>
     )
