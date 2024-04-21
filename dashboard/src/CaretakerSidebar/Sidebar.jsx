1cb22990318e62c07345584457bc856ab7cc3a5c
Himanshu <hbansal_be21@thapar.edu>
2024-04-21 06:30:43 +0530

    owasp

---
+++ dashboard/src/CaretakerSidebar/Sidebar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import './Sidebar.css';
+import { Link } from 'react-router-dom';
+
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+      <div className="sidebar-header">
+        <h3>Caretaker Portal</h3>
+        <button onClick={toggle}><FaTimes /></button>
+      </div>
+      <ul className="sidebar-menu">
+        <li><Link to="/alerts">Alerts</Link></li>
+        {/* <li><Link to="/patientOut">Patient Out</Link></li> */}
+        {/* <li><Link to="/unknownPersonAlert">Unknown Person Alert</Link></li> */}
+        <li><Link to="/reports">Upload Reports</Link></li>
+        <li><Link to="/contactGuardian">Contact Guardian</Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
